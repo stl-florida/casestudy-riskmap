d2e32cd9450d1ec0a91c7fb48ca4589189d28326
ojha-url <ojha@mit.edu>
2017-07-31 20:52:42 -0400

    Removed parameterized route from URL, use ? query param risk

--- src/app.js
+++ src/app.js
@@ -2,12 +2,9 @@ export class App {
   configureRouter(config, router) {
     config.title = 'RiskMap';
     config.options.pushState = true;
-    config.options.root = '';
+    config.options.root = '/';
     config.map([
-      {route: '/:risk', name: 'map', moduleId: 'routes/riskmap/riskmap'}/*,
-      {route: 'stormsurge',   name: 'stormsurge',  moduleId: 'routes/storm/storm'},
-      {route: 'flood',   name: 'flood',  moduleId: 'routes/flood/flood'},
-      {route: 'groundwater',   name: 'groundwater',  moduleId: 'routes/gw/gw'}*/
+      {route: '', name: 'map', moduleId: 'routes/riskmap/riskmap'}
     ]);
     config.mapUnknownRoutes({redirect: 'map'});
     this.router = router;
