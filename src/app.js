8f26515784c88a7b7dcb7b8a5f8ea52c27c449e8
ojha-url <ojha@mit.edu>
2017-07-31 20:46:53 -0400

    Added root '/' ahead of risk param

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ export class App {
     config.options.pushState = true;
     config.options.root = '';
     config.map([
-      {route: ':risk', name: 'map', moduleId: 'routes/riskmap/riskmap'}/*,
+      {route: '/:risk', name: 'map', moduleId: 'routes/riskmap/riskmap'}/*,
       {route: 'stormsurge',   name: 'stormsurge',  moduleId: 'routes/storm/storm'},
       {route: 'flood',   name: 'flood',  moduleId: 'routes/flood/flood'},
       {route: 'groundwater',   name: 'groundwater',  moduleId: 'routes/gw/gw'}*/
